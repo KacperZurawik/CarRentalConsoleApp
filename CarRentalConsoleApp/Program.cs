7a85a123f3e6ebd72bd0192bce40b62b3ec4c991
KacperZurawik <kaacper181@gmail.com>
2024-11-10 08:47:15 +0100

    Added switch case menu

--- CarRentalConsoleApp/Program.cs
+++ CarRentalConsoleApp/Program.cs
@@ -10,6 +10,38 @@ namespace CarRentalConsoleApp
     {
         static void Main(string[] args)
         {
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Choose an option: ");
+                Console.WriteLine("1. Rent a car");
+                Console.WriteLine("2. Return a car");
+                Console.WriteLine("3. Customers");
+                Console.WriteLine("4. Cars");
+                Console.WriteLine("5. Extit");
+
+                string menuOptionChoice = Console.ReadLine();
+
+                switch (menuOptionChoice)
+                {
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        break;
+
+                    case "5":
+                        running = false;
+                        break;
+                }
+            }
         }
     }
 }
