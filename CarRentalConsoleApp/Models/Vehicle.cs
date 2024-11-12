50a9ecce3cb860d0fe22f4baf78d76a8850b3037
KacperZurawik <kaacper181@gmail.com>
2024-11-12 20:17:26 +0100

    Updated Vehicle class by adding PlateNumber property

---
+++ CarRentalConsoleApp/Models/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalConsoleApp.Models
+{
+    internal class Vehicle
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Vin { get; set; }
+        public string PlateNumber { get; set; }
+
+        public Vehicle(int id, string make, string model, int year, int vin, string plateNumber)
+        {
+            Id = id;
+            Make = make;
+            Model = model;
+            Year = year;
+            Vin = vin;
+            PlateNumber = plateNumber;
+        }
+    }
+}
+    
